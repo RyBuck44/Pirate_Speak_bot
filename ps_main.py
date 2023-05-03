2c00d8030da2658f85534edcb84ef5157a620437
RyBuck44 <bucklerryan2@gmail.com>
2023-05-03 13:51:28 -0400

    outlined bot, added run, added help slash command

--- ps_main.py
+++ ps_main.py
@@ -3,3 +3,29 @@ from discord import app_commands
 from discord.ext import commands
 import os
 
+secret_code = os.getenv('PS_TOKEN')
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+
+class ps(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.synced = False
+
+    async def on_ready(self):
+        await tree.sync()
+        self.synced = True
+        print('Bot is online')
+
+
+bot = ps()
+tree = app_commands.CommandTree(bot)
+
+
+@tree.command(name='ahoy', description='ARG! Shows ya how I work matey!')
+async def self(interaction: discord.Interaction):
+    await interaction.response.send_message('Time for a round of vinegar talk!\n'
+                                            'Use /insult whenever yarrrrr ready you scallywag!')
+
+
+bot.run(secret_code)
